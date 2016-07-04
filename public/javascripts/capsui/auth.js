b10d216d21ebbcbcd55e91dab57464906e377865
tancw <changwei114@126.com>
2016-07-04 16:04:38 +0800

    引入注销

--- public/javascripts/capsui/auth.js
+++ public/javascripts/capsui/auth.js
@@ -2,7 +2,7 @@
  * Created by tancw on 2016/6/23.
  */
 //全局变量
-app.constant('AUTH_EVENTS',{
+app.constant('AUTH_EVENTS', {
     loginSuccess: 'auth-login-success',
     loginFailed: 'auth-login-failed',
     logoutSuccess: 'auth-logout-success',
@@ -31,9 +31,10 @@ app.factory('AuthService', function ($http, Session) {
                 return res.data.user;
             });
     };
-    
-    authService.logout = function () {
+
+    authService.logout = function (callback) {
         Session.destroy();
+        return callback();
     }
 
     authService.isAuthenticated = function () {
@@ -51,7 +52,7 @@ app.factory('AuthService', function ($http, Session) {
 })
 
 app.service('Session', function () {
-    this.create = function (sessionId, userId, userRole,access_token) {
+    this.create = function (sessionId, userId, userRole, access_token) {
         this.id = sessionId;
         this.userId = userId;
         this.userRole = userRole;
@@ -66,34 +67,39 @@ app.service('Session', function () {
     return this;
 })
 
-app.controller("layoutController",function ($scope,USER_ROLES,AuthService) {
+app.controller("layoutController", function ($scope, USER_ROLES, AUTH_EVENTS, AuthService) {
     $scope.currentUser = null;
     $scope.userRoles = USER_ROLES;
     $scope.isAuthorized = AuthService.isAuthorized;
     $scope.setCurrentUser = function (user) {
         $scope.currentUser = user;
     };
+
+    $scope.logoutCurrnetuser = function () {
+        $scope.currentUser = null;
+    }
 })
 
 
-app.controller('LoginController',function ($scope, $rootScope, AUTH_EVENTS, AuthService,$state) {
-   $scope.credentials = {
-       username:'',
-       password:''
-   };
+app.controller('LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService, $state) {
+    $scope.credentials = {
+        username: '',
+        password: ''
+    };
     $scope.login = function () {
         AuthService.login($scope.credentials).then(function (user) {
             $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
             $scope.setCurrentUser(user);
             $state.go('temps');
-        },function () {
+        }, function () {
             $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
         });
     };
-    
+
     $scope.logout = function () {
-        AuthService.logout().then(function () {
+        AuthService.logout(function () {
             $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+            $scope.logoutCurrnetuser();
         });
     }
 });
